b4c045c92a38d96b4d9d755f88b9a53ff83d01c8
mohamedelazab86 <r_mido99@yahoo.com>
2023-12-04 17:25:26 +0200

    add summernote editor

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Product,Brand,Image,Review
+from django_summernote.admin import SummernoteModelAdmin
+
+class Productadmin(SummernoteModelAdmin):
+    list_display=['name','price','flag']
+    list_filter=['brand']
+    search_fields=['name']
+    summernote_fields = ('subtitle','descriptions')
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product,Productadmin)
 admin.site.register(Brand)
 admin.site.register(Image)
 admin.site.register(Review)
