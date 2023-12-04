6dd59a6e0b762592013c31a99001aeea06169d02
mohamedelazab86 <r_mido99@yahoo.com>
2023-12-04 17:30:55 +0200

    add tabularinline

--- products/admin.py
+++ products/admin.py
@@ -2,14 +2,19 @@ from django.contrib import admin
 from .models import Product,Brand,Image,Review
 from django_summernote.admin import SummernoteModelAdmin
 
+class Imageadmin(admin.TabularInline):
+    model=Image
+
 class Productadmin(SummernoteModelAdmin):
     list_display=['name','price','flag']
     list_filter=['brand']
     search_fields=['name']
     summernote_fields = ('subtitle','descriptions')
+    inlines=[Imageadmin,]
+
 
 # Register your models here.
 admin.site.register(Product,Productadmin)
 admin.site.register(Brand)
-admin.site.register(Image)
+#admin.site.register(Image)
 admin.site.register(Review)
